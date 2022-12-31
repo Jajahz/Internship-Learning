f8e2befd395cdcc5a6effcf059cd77472f9f064a
Jajahz <jajah.asmp@gmail.com>
2022-12-31 18:01:46 +0700

    [Update] Second clip - day 2

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: 'http://facebook.com',
       showBooks: true,
-      title: 'The Final Empire',
-      author: 'Brandon Sanderson',
-      age: 45,
+      x: 0,
+      y: 0,
+      books: [
+        {
+          id: 1,
+          title: 'Book 1',
+          author: 'Author 1',
+          img: 'assets/1.png',
+          isFav: true,
+        },
+        {
+          id: 2,
+          title: 'Book 2',
+          author: 'Author 2',
+          img: 'assets/2.png',
+          isFav: false,
+        },
+        {
+          id: 3,
+          title: 'Book 3',
+          author: 'Author 3',
+          img: 'assets/3.png',
+          isFav: true,
+        },
+      ],
     }
   },
   methods: {
@@ -13,6 +36,21 @@ const app = Vue.createApp({
     },
     toggleShowBooks() {
       this.showBooks = !this.showBooks
+    },
+    handleEvent(e) {
+      console.log(e, e.type)
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX
+      this.y = e.offsetY
+    },
+    changeFav(book) {
+      book.isFav = !book.isFav
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav)
     },
   },
 })
