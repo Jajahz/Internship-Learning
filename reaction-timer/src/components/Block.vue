8847157b3bc4100cefe2e7013f7ee5be6b798319
Jajahz <jajah.asmp@gmail.com>
2023-01-07 01:02:22 +0700

    [Update] Fifth clip - day 5

---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  props: ['delay'],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    }
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10
+      }, 10)
+    },
+    stopTimer() {
+      clearInterval(this.timer)
+      this.$emit('end', this.reactionTime)
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true
+      this.startTimer()
+    }, this.delay)
+  },
+}
+</script>
+
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
+</template>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: pink;
+  color: whitesmoke;
+  text-align: center;
+  padding: 100px 0;
+  margin: 49px auto;
+}
+</style>
---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null,
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja Finger'
+    } else if (this.score < 400) {
+      this.rank = 'Rapid Reflexes'
+    } else {
+      this.rank = 'Snail pace...'
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <p>Reaction Time: {{ score }} ms</p>
+    <p class="rank">Your Rank is: {{ rank }}</p>
+  </div>
+</template>
+
+<style>
+.rank {
+  font-size: 1.4 em;
+  color: violet;
+  font-weight: bold;
+}
+</style>
